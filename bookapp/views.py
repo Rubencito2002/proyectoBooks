4d7de6b921f411c0a265daf9eae8fc0bce1712b8
Rubencito2002 <93660491+Rubencito2002@users.noreply.github.com>
2023-11-16 15:08:24 +0100

    creaccion views

--- bookapp/views.py
+++ bookapp/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+from .models import Book
+from django.forms import BookForm
 
-# Create your views here.
+class BookList(View):
+    bookList_template = 'book/book_list.html'
+    def get(self, request):
+        books = Book.objects.all()
+        return render(request, self.bookList_template , {'books': Book})
+
+class BookDetail(View):
+    bookDetail_template = 'book/book_detail.html'
+    def get(self, request, id):
+        books = Book.objects.get(id=id)
+        return render(request, self.bookDetail_template , {'books': Book})
+
+class BookCreate(View):
+    bookForm_template = 'book/book_form.html'
+    def get(self, request):
+        form = BookForm()
+        return render(request, self.bookForm_template , {'form': form})
+
+    def post(self, request):
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+        return render(request, self.bookForm_template , {'form': form})
