343b9470ccb2c04ae18b73c51f98a73900e9afbc
Rubencito2002 <93660491+Rubencito2002@users.noreply.github.com>
2023-11-16 14:49:40 +0100

    creacion del modelo y update bbdd

--- bookapp/models.py
+++ bookapp/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator
 from django.db import models
+from django.db.models import Model, CASCADE
 
-# Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=150)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = models.CharField(max_length=500)
+    rating = models.PositiveIntegerField(validators=[MaxValueValidator(5)])
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
