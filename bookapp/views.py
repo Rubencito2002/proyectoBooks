13451d3057bbfab0d9be3231c8cf5c87ec99968d
Rubencito2002 <ruben.machado1920@gmail.com>
2023-11-17 11:28:10 +0100

    arreglando fallos

--- bookapp/views.py
+++ bookapp/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, redirect
 from django.views import View
 from .models import Book
 from .forms import BookForm
+from django.views.generic import ListView, DetailView
 
+"""
 class BookList(View):
     nombre_template = 'book/book_list.html'
 
@@ -10,19 +12,21 @@ class BookList(View):
         books = Book.objects.all()
         form = BookForm()
         return render(request, self.nombre_template, {'books': books})
-"""
-def BookList(request):
-    books = Book.objects.all()
-    form = BookForm()
-    return render(request, 'book/book_list.html', {'books': books})
-"""
 
 class BookDetail(View):
-    bookDetail_template = 'book/book_detail.html'
+    bookDetail_template = 'book/book_details.html'
 
     def get(self, request, id):
         books = Book.objects.get(id=id)
         return render(request, self.bookDetail_template , {'books': books})
+"""
+
+class BookList(ListView):
+    model = Book
+
+class BookDetail(DetailView):
+    model = Book
+    bookDetail_template = 'book/book_details.html'
 
 class BookCreate(View):
     books = Book.objects.all()
